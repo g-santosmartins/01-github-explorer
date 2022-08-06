1309b6aff9f642784b3fe0ea452d363dbf997e1e
Guilherme Martins <g.santosmartins@outlook.com>
2022-08-06 15:37:58 -0300

    feat: adding git repos from Github using hooks

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,33 @@
+import { useEffect, useState } from "react"
+import "../styles/repositories.scss"
+
 import { RepositoryItem } from "./RepositoryItem"
 
-
-import "../styles/repositories.scss"
 export function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([])
+
   const repository =  {
     name: "unform",
     description: "Forms in React",
-    link: "https://github.com/g-santosmartins"
+    link: "https://api.github.com/users/g-santosmartins/repos"
   }
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+    .then(response => response.json() )
+    .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map((repository, index) => {
+          return <RepositoryItem key={index} repository={repository}/>
+        })}
+      
       </ul>
     </section>
   )

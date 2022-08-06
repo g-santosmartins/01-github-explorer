72958b5522d66b69225985a432eff6cf29f9454e
Guilherme Martins <g.santosmartins@outlook.com>
2022-08-06 14:38:21 -0300

    feat: adding repository item component

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,36 +1,22 @@
+import { RepositoryItem } from "./RepositoryItem"
+
+
+
 export function RepositoryList() {
-  const RepositoryName = 'unform2'
+  const repository =  {
+    name: "unform",
+    description: "Forms in React",
+    link: "https://github.com/g-santosmartins"
+  }
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>{RepositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
       </ul>
     </section>
   )
